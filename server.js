caecbcbea07bf434c06c09b9a64ffa32ee6f4a1a
Arimantas jurjonas <arimantaz@gmail.com>
2022-04-30 10:43:44 +0300

    laikas

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-app.listen(3000)
+// app.listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.listen(5500, () => {
+    console.log('serveris paleistas!');
+});
+
+app.get('/time', (request, response) => {
+  const date = new Date();
+  const dateString = date.toLocaleDateString();
+  const timeString = date.toLocaleTimeString();
+  response.send(`${dateString} ${timeString}`);
+});
